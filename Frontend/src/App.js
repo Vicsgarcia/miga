aa49319db6d1cddda05ed6f6c8b98f94c11063cd
Victor Garcia <v.garcia.mestre@gmail.com>
2019-07-06 14:47:52 +0200

    App.js, index.js and components created and connected

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row mt-5">
+          <div className="col-12">
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ Frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default () => {
+  return (
+    <div className="box mx-auto">
+      <div className="row">
+        <div className="col-6">
+          <h3>Miga</h3>
+          <p className="lead mb-5">La web de Miga</p>
+        </div>
+        <div className="col-6"></div>
+      </div>
+    </div>
+  );
+}
